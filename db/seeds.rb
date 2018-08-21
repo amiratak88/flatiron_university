934eab953227755b23b775a7779b8f6769f72141
github username <github email address>
2018-08-21 14:02:55 -0400

    starting_the_seed_file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,117 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#Film Courses
+Course.create(title: "Sound on Film", description: "The final course in the sound concentration sequence, this course is focused on composing and sound designing a longer-form capstone work in collaboration with a filmmaker also in the MA program. Final grade is based on the quality of the finished product and an evaluation by the instructor of how the student incorporated knowledge and techniques introduced in the previous three classes.")
+Course.create(title: , description: )
+Course.create(title: , description: )
+#Engineering Courses
+Course.create(title: , description: )
+Course.create(title: , description: )
+Course.create(title: , description: )
+#Government
+Course.create(title: "Comparative Politics and Policy", description: "This course provides the analytical knowledge and practical skills to understand comparative politics and policy worldwide.  It addresses a range of foundational topics:  (i) Concepts, theories, evidence, and methods in comparative politics; (ii) Classifying varieties of democratic and autocratic regimes, and processes of regime transition and consolidation; (iii) Institutional designs, including electoral systems, party systems, types of executives, and federalism; (iv) Channels of mass mobilization including through voting, protest activism, civil society organizations, media, civil disobedience, terrorism, and revolutionary upheavals; and (v) The performance of governance and public policies.  The course covers these issues by utilizing the methods and techniques of comparative politics. You will learn about polities worldwide – as well as thereby enriching and deepening your understanding of your own nation.")
+Course.create(title: "Advanced Microeconomic Analysis II", description: "This is the second semester of a rigorous two-semester sequence in advanced microeconomic analysis for MPA/ID students. Topics covered include general equilibrium, externalities and public goods, welfare economics, game theory, economics of information, incentives, and contracts. Theory is illustrated by relevant applications to international development and other areas.
+
+Prerequisites: Multivariate calculus and constrained optimization; intermediate microeconomics. This course is open to non-MPA/ID students only by permission of instructor. API-110 and API-112 cannot both be taken for credit. Students may receive credit for both API-110 and API-102 or API-303 only if API-102 or API-303 is taken first." )
+Course.create(title: , description: )
+
+#Students
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+Student.create(first_name: , last_name: , gender: , ethnicity: , dob: , financial_aid: , major_id: )
+
+#Professors
+Professor.create(first_name: "Glenfield", last_name: "Payne", gender: "M", ethnicity: "Black", dob: Date.new (1965, 8, 10))
+Professor.create(first_name: , last_name: , gender: , ethnicity: , dob: )
+Professor.create(first_name: , last_name: , gender: , ethnicity: , dob: )
+Professor.create(first_name: , last_name: , gender: , ethnicity: , dob: )
+Professor.create(first_name: "Jie" , last_name: "Bai", gender: "F", ethnicity: "Asian" , dob: Date.new (1980, 11, 30))
+Professor.create(first_name: "Pippa", last_name: "Norris", gender: "F", ethnicity: "Caucasian", dob: Date.new (1939, 1, 1))
+
+
+#Sections
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+Section.create(day: , start_time: , end_time: , professor_course_id: )
+
+# ProfessorCourses
+ProfessorCourse.create(professor_id: 1, course_id: 1, location: "Room 11", start_date: Date.new (2018, 9, 5), end_date: Date.new (2018, 12, 10))
+ProfessorCourse.create(professor_id: , course_id: 2, location: , start_date: , end_date: )
+ProfessorCourse.create(professor_id: , course_id: 3, location: , start_date: , end_date: )
+ProfessorCourse.create(professor_id: , course_id: 4, location: , start_date: , end_date: )
+ProfessorCourse.create(professor_id: , course_id: 5, location: , start_date: , end_date: )
+ProfessorCourse.create(professor_id: , course_id: 6, location: , start_date: , end_date: )
+ProfessorCourse.create(professor_id: 6 , course_id: 7, location: "Room 67", start_date: Date.new (2018, 9, 5), end_date: Date.new (2018, 12, 10))
+ProfessorCourse.create(professor_id: 5, course_id: 8, location: "Room 58", start_date: Date.new (2019, 1, 25), end_date: Date.new (2019, 5, 12))
+ProfessorCourse.create(professor_id: , course_id: 9, location: , start_date: , end_date: )
+
+#StudentProfessorCourses
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+StudentProfessorCourses.create(professor_course_id: , student_id: , grade: )
+
+#Requirements
+Requirement.create(major_id: 1, course_id: )
+Requirement.create(major_id: 1, course_id: )
+Requirement.create(major_id: 1, course_id: )
+Requirement.create(major_id: 1, course_id: )
+Requirement.create(major_id: 2, course_id: )
+Requirement.create(major_id: 2, course_id: )
+Requirement.create(major_id: 2, course_id: )
+Requirement.create(major_id: 2, course_id: )
+Requirement.create(major_id: 3, course_id: )
+Requirement.create(major_id: 3, course_id: 7)
+Requirement.create(major_id: 3, course_id: 8)
+Requirement.create(major_id: 3, course_id: 1)
+
+#Majors
+Major.create(title: "Film & Media", description: "")
+Major.create(title: , description: )
+Major.create(title: "Government", description: "The graduate school of public policy offers you the opportunity to learn in an atmosphere of inquiry and open dialogue through a comprehensive range of degree and Executive Education programs. The HKS experience is a vibrant exchange of ideas and expertise, a give and take of energy and inspiration. Whether your goal is to earn a graduate degree or gain new skills and knowledge through an executive program, you will learn from our faculty of renowned scholars and seasoned practitioners while engaging with visiting luminaries, global thought leaders, and talented classmates.")
