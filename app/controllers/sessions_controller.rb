da4dc0a00e980eda1fadb2cfb223fa2e218c4c31
github username <github email address>
2018-08-21 23:11:13 -0400

    finished student page - added errors for login page - added progress bar - added some more css - created logic for major show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
 class SessionsController < ApplicationController
-    
+
     def new
     end
 
     def create
         # This code is expensive. Sql could be used to make the code more efficient.
-        
+
         @current = (Student.all + Professor.all).find{ |inst| inst.username == params[:username] }
 
         if @current && @current.authenticate(params[:password])
             redirect_to @current
         else
+            flash[:errors] = ["Please enter a username and a password."]
             redirect_to login_path
         end
     end
--- app/models/student.rb
+++ app/models/student.rb
@@ -7,20 +7,70 @@ class Student < ApplicationRecord
     has_secure_password
     validate :cross_table_uniqueness
 
-    def cross_table_uniqueness
-        condition1 = Student.find_by(username: username)
-        condition2 = Professor.find_by(username: username)
-        errors.add(:username, 'Username already exists') if condition1 || condition2
+    def full_name
+      "#{first_name} #{last_name}"
+    end
+
+    def overall_average
+      gpa = 0
+      total = self.student_professor_courses.map do |course|
+      case course.grade
+        when "A+"
+          gpa = 4
+        when "A"
+          gpa = 4.0
+        when "A-"
+          gpa = 3.7
+        when "B+"
+          gpa = 3.3
+        when "B"
+          gpa = 3.0
+        when "B-"
+          gpa = 2.7
+        when "C+"
+          gpa = 2.3
+        when "C"
+          gpa = 2.0
+        when "C-"
+          gpa = 1.7
+        when "D+"
+          gpa = 1.3
+        when "D"
+          gpa = 1.0
+        when "F"
+          gpa = 0.0
+        end
+      end
+      sum = total.inject(0) { |result, element| result + element }
+      sum.to_f/total.length
+    end
+
+  def required_courses
+    self.major.requirements.map do |requirement|
+      Course.find(requirement.course_id).title
+    end
+  end
+
+  def courses_taken
+    self.courses.map do |course|
+      Course.find(course.id).title
+    end
+  end
+
+  def cross_table_uniqueness
+      condition1 = Student.find_by(username: username)
+      condition2 = Professor.find_by(username: username)
+      errors.add(:username, 'Username already exists') if condition1 || condition2
 
 
-        # It works but it's slower compared to the current solution since it's using Ruby instead of sql.
+      # It works but it's slower compared to the current solution since it's using Ruby instead of sql.
 
-        # v = (Student.all + Professor.all).map do |user|
-        #     user.username
-        # end.include?(self.username)
+      # v = (Student.all + Professor.all).map do |user|
+      #     user.username
+      # end.include?(self.username)
 
-        # if v
-        #     errors.add(:username, 'Username is taken')
-        # end
+      # if v
+      #     errors.add(:username, 'Username is taken')
+      # end
     end
 end
